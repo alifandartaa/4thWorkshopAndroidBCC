54181e7bd0f7cccd5c11a91fa3c42443db833279
Alif Andarta <alifandartaa@yahoo.com>
2020-04-18 16:32:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/practicetablayout/SectionPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.practicetablayout;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.Objects;
+
+public class SectionPagerAdapter extends FragmentPagerAdapter {
+
+    private final Context mContext;
+
+    public SectionPagerAdapter(Context context, @NonNull FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mContext = context;
+    }
+
+    @StringRes
+    private final int[] TAB_TITLES = new int[]{
+            R.string.tab_text_1,
+            R.string.tab_text_2
+    };
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new CallFragment();
+                break;
+            case 1:
+                fragment = new ContactFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
